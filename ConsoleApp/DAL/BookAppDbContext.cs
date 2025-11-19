da8eeb0d8cb9bbbe3bfc3f609da5647b4b8fb8ea
Kurtulus Yuksel <kurtulusyuksel@gmail.com>
2025-11-19 20:11:23 +0300

    changed

---
+++ ConsoleApp/DAL/BookAppDbContext.cs
@@ -0,0 +1,27 @@
+﻿using ConsoleApp.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp.DAL
+{
+    public class BookAppDbContext:DbContext
+    {
+        public DbSet<Book> Books { get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+
+            optionsBuilder.UseSqlServer("Data Source=(localdb)\\mssqllocaldb;Initial Catalog=BookAppDb;");
+            
+        }
+
+
+
+
+
+
+    }
+}
---
+++ ConsoleApp/Entities/Book.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp.Entities
+{
+    public class Book
+    {
+        [Key]
+        public int BookId { get; set; }
+        [Required] //gerekli data annatotion çok tercih edilen bir yöntem değil
+        [MaxLength(50)]
+        public string Title { get; set; }
+
+
+
+    }
+}
